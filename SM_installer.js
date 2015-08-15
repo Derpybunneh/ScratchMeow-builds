0e01fd3eace9f6796bf80fe97cef6a4b04e0face
Andrew <habitroblox123@gmail.com>
2015-08-15 18:01:24 -0400

    Update SM_installer.js

--- SM_installer.js
+++ SM_installer.js
@@ -1,4 +1,4 @@
-ScratchExtensions.loadExternalJS('https://raw.githubusercontent.com/mcpro09/ScratchMeow-builds/master/SM_browser.js');
+ScratchExtensions.loadExternalJS('https://cdn.rawgit.com/mcpro09/ScratchMeow-builds/master/SM_browser.js');
 ScratchExtensions.loadExternalJS('https://cdn.rawgit.com/mcpro09/ScratchMeow-builds/master/SM_storage.js');
 ScratchExtensions.loadExternalJS('https://cdn.rawgit.com/mcpro09/ScratchMeow-builds/master/SM_weather.js');
 ScratchExtensions.loadExternalJS('https://cdn.rawgit.com/mcpro09/ScratchMeow-builds/master/SM_speech.js');
