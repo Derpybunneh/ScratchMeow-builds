326279488ee8742589789b6ac0c222eb594a7078
Andrew <habitroblox123@gmail.com>
2015-08-15 15:05:13 -0400

    Update SM_installer.js

--- SM_installer.js
+++ SM_installer.js
@@ -1,4 +1,4 @@
-ScratchExtensions.loadExternalJS('https://cdn.rawgit.com/mcpro09/835691be84e257a846cd/raw/089d48565e7aa56cfda860492ab7ddaecafdf0fa/SM_browser.js');
-ScratchExtensions.loadExternalJS('https://cdn.rawgit.com/mcpro09/36c82b8f74efbffb949e/raw/b9d07df1754d7e92ab5d4a2d0a630dcac1a0707d/SM_storage.js');
-ScratchExtensions.loadExternalJS('https://cdn.rawgit.com/mcpro09/63cc88d3dfa7447c847e/raw/b5639fe2d004943d9fbdca71d823411dab92ce1b/SM_weather.js');
-ScratchExtensions.loadExternalJS('https://cdn.rawgit.com/mcpro09/9565f2e981bfadf46f2a/raw/b7646c6f1778ab151c94240f3bd60b3bdb4a85b4/SM_speech.js');
+ScratchExtensions.loadExternalJS('https://cdn.rawgit.com/mcpro09/ScratchMeow-builds/master/SM_browser.js');
+ScratchExtensions.loadExternalJS('https://cdn.rawgit.com/mcpro09/ScratchMeow-builds/master/SM_storage.js');
+ScratchExtensions.loadExternalJS('https://cdn.rawgit.com/mcpro09/ScratchMeow-builds/master/SM_weather.js');
+ScratchExtensions.loadExternalJS('https://cdn.rawgit.com/mcpro09/ScratchMeow-builds/master/SM_speech.js');
